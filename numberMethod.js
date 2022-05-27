e5cf16d042b7e4c3941dc2a4fde356e377daca08
samia01 <48171304+samia01@users.noreply.github.com>
2022-05-27 19:09:34 +0600

    Add files via upload
    JS (Number Method)

---
+++ numberMethod.js
@@ -0,0 +1,9 @@
+var number =2.5789;
+console.log(typeof(number));
+var numbe =2.5;
+var r=toString(numbe);
+console.log(typeof(r));
+console.log(Number(number));
+console.log(typeof(number));
+console.log(number.toFixed(1));
+console.log(number.toPrecision(2));
